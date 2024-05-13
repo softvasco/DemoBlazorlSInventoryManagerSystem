35d2c04ef58ea320f1ab18e2a04a9de2e77f3575
softvasco <sprvasco@hotmail.com>
2024-05-13 15:01:43 +0100

    Setup dependency injection for auth add sql connection string

---
+++ Infrastructure/DataAccess/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Application.Extension.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.DataAccess
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : 
+        IdentityDbContext<ApplicationUser>(options)
+    {
+    }
+}
